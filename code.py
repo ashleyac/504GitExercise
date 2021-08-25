682a58ba656494dc8208cb00498d1db2550eee93
ashleyac <ashleyac@umich.edu>
2021-08-25 10:14:52 -0400

    readability

--- code.py
+++ code.py
@@ -1,16 +1,16 @@
-def function1(a):
-    b = dict()
-    for c in a:
-        if c not in b:
-            b[c] = 1
+def base_count(sequence):
+    count_dict = dict()
+    for base in sequence:
+        if base not in count_dict:
+            count_dict[base] = 1
         else:
-            b[c] += 1
-    return b
+            count_dict[base] += 1
+    return count_dict
 
-def function2(a):
+def base_freq(count_dict):
     print('freqs')
-    total = float(sum([a[b] for b in a.keys()]))
-    for b in a.keys():
-        print(b + ':' + str(a[b]/total))
+    total = float(sum([count_dict[base] for base in count_dict.keys()]))
+    for base in count_dict.keys():
+        print(base + ':' + str(count_dict[base]/total))
 
-function2(function1('ATCTGACGCGCGCCGC'))
+base_freq(base_count('ATCTGACGCGCGCCGC'))
